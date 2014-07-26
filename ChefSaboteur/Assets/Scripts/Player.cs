9f985845fde8bf52b8d7eb2a6bfc777ccdb6a167
Patrick Morgan <p.w.morgan@gmail.com>
2014-07-25 23:42:51 -0700

    Added input bindings.

--- ChefSaboteur/Assets/Scripts/Player.cs
+++ ChefSaboteur/Assets/Scripts/Player.cs
@@ -22,8 +22,8 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxis (leftVerticalAxis)) {
-			Debug.Log ();
+		if (Input.GetAxis (leftVerticalAxis) != 0) {
+			Debug.Log ("Detected Left Hand move);
 		}
 	}
 }
