c981c645e677ec962d6e254e2baa865966f4da82
Patrick Morgan <p.w.morgan@gmail.com>
2014-07-25 23:15:02 -0700

    Initial commit of all the project files.

---
+++ ChefSaboteur/Assets/Scripts/Hand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+
+	private GameObject _heldObject;
+	private Vector3 _position;
+	private Vector3 _velocity;
+	private	bool _isExtending;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// If holding an object
+		// Move object to position;
+
+	}
+}
---
+++ ChefSaboteur/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Hand lefthand;
+	public Hand righthand;
+
+	public string leftVerticalAxis;
+	public string leftHorizontalAxis;
+	public string rightVerticalAxis;
+	public string rightHorizontalAxis;
+	public string leftHandButton;
+	public string rightHandButton;
+	public string actionButton;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis (leftVerticalAxis)) {
+			Debug.Log ();
+		}
+	}
+}
