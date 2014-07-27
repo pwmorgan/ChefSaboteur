be6c2a0c432e97c56408657e52ad12fba87e8c01
Patrick Morgan <p.w.morgan@gmail.com>
2014-07-27 16:48:32 -0700

    Title screen stuff.

---
+++ ChefSaboteur/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	private bool _isStarting = false;
+	private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = transform.GetComponent<Animator> () as Animator;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_isStarting) {
+			if (Input.GetButtonDown("joystick button 16")) {
+				_animator.Play();
+			}
+		}
+	}
+}
